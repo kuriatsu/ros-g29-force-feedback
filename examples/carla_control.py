cacf867fc45e2a97267249cb0d6daf47c5b8b8df
kuriatsu <kuriatsubayashi712@gmail.com>
2022-11-06 17:31:02 +0900

    finish test

--- examples/carla_control.py
+++ examples/carla_control.py
@@ -21,35 +21,35 @@ import numpy as np
 import random
 import rclpy
 from rclpy.node import Node
-import ros_g29_force_feedback.msg import force_feedback
+from ros_g29_force_feedback.msg import ForceFeedback
 
 class CarlaControlNode(Node):
     def __init__(self, client):
         super().__init__("carla_ff_node")
-        self.publisher = self.create_publisher(force_feedback,"/ff_target", 10)
+        self.publisher = self.create_publisher(ForceFeedback,"/ff_target", 10)
         self.timer = self.create_timer(0.1, self.timer_cb)
         self.client = client
         self.world = client.get_world()
         self.actor = self.get_hero()
 
-    def get_hero():
+    def get_hero(self):
         for actor in self.world.get_actors():
             if actor.attributes.get("role_name") in ["hero", "ego_vehicle"]:
                 return actor
             
     def timer_cb(self):
-        out_msg = force_feedback()
+        out_msg = ForceFeedback()
         out_msg.header.stamp = self.get_clock().now().to_msg()
         
         steering_angle = self.actor.get_control().steer
-        out_msg.position = -steering_angle
+        out_msg.position = steering_angle
         out_msg.torque = 0.8
         self.publisher.publish(out_msg)
 
 def main(args=None):
 
     rclpy.init(args=args)
-    client = carla.Client("127.0.0.0", 2000)
+    client = carla.Client("127.0.0.1", 2000)
     client.set_timeout(2.0)
 
     carla_control_node = CarlaControlNode(client)
